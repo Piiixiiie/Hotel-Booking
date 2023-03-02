3a72719337ca62f97a642bddd4dae487a24648e9
John Rowntree <skyizda@outlook.com>
2023-03-02 17:35:57 +0000

    02/03/2023 - New Code Added, Tested lightly, Do loop is stuck or noty working for Double or fam suites, need second opinion.

--- Module1.vb
+++ Module1.vb
@@ -3,13 +3,19 @@
     Sub Main()
         Dim famName As String
         Dim arrDate As String
-        Dim amtRooms As Double
+        Dim duration As String
         Dim sinRm As Integer
         Dim douRm As Integer
         Dim famSu As Integer
-        Dim Total As Double
-        Dim rooms As Integer
-        Dim ans As Boolean
+        Dim sinRmT As Integer
+        Dim douRmT As Integer
+        Dim famSuT As Integer
+        Dim roomT As Integer
+        Dim nTotal As Double
+        Dim discount As Double
+        Dim aTotal As Double
+        Dim VATTotal As Double
+
 
         Console.WriteLine("Hello what is the name the rooms will be under?")
         famName = Console.ReadLine()
@@ -18,10 +24,11 @@
         Console.WriteLine("So what will the arrival date be " & famName & "?")
         arrDate = Console.ReadLine()
 
-        Console.WriteLine("And how many rooms? Please note we have a max of 4 per booking.")
-        amtRooms = Console.ReadLine()
+        Console.WriteLine("And how long would you like to stay with us for? We have up to 2 weeks free")
+        Console.WriteLine("Please enter the amount of days you would like to stay with us for.")
+        duration = Console.ReadLine()
 
-        Console.WriteLine("Below are our prices for each type of room per night.")
+        Console.WriteLine("Amazing, so below are our prices for each type of room per night.")
 
         Console.WriteLine("Single Room - £47")
         Console.WriteLine("Double Room - £90")
@@ -31,16 +38,64 @@
         Console.WriteLine("Please choose which kind of rooms you will require.")
 
 
+        Do While roomT <= 5
+            roomT = (sinRm + douRm + famSu)
 
-        Console.WriteLine("How many Single Room(s) do you require?")
-        sinRm = Console.ReadLine()
+            If roomT >= 5 Then
+                Console.WriteLine("Please rememebr we have a 4 Rooms per booking limit. Thank You.")
+            End If
 
-        Console.WriteLine("And how many Double Room(s) do you require?")
-        douRm = Console.ReadLine()
+            Console.WriteLine("How many Single Room(s) do you require?")
+            sinRm = Console.ReadLine()
 
-        Console.WriteLine("And Fianlly how many Family Suite(s) do you require?")
-        famSu = Console.ReadLine()
+            Console.WriteLine("And how many Double Room(s) do you require?")
+            douRm = Console.ReadLine()
 
+            Console.WriteLine("And finally how many Family Suite(s) do you require?")
+            famSu = Console.ReadLine()
+
+        Loop
+
+
+
+        Console.WriteLine("Thank you for choosing to stay with us, below your total will be displayed.")
+
+
+        If roomT >= 3 & duration >= 7 Then
+
+            Console.WriteLine("You have booked" & sinRm & "Single room(s)")
+            Console.WriteLine("You have booked" & douRm & "Double room(s)")
+            Console.WriteLine("You have booked" & famSu & "Family Suite(s)")
+            Console.WriteLine("You are eligible for a discount of 10% ")
+
+            sinRmT = (sinRm * 47)
+            douRmT = (douRm * 90)
+            famSuT = (famSu * 250)
+
+            nTotal = (sinRmT + douRmT + famSuT)
+            aTotal = (nTotal * duration)
+            discount = (aTotal * 0.9)
+            VATTotal = (discount * 1.2)
+
+        Else
+
+            Console.WriteLine("You have booked" & sinRm & "Single room(s)")
+            Console.WriteLine("You have booked" & douRm & "Double room(s)")
+            Console.WriteLine("You have booked" & famSu & "Family Suite(s)")
+
+            sinRmT = (sinRm * 47)
+            douRmT = (douRm * 90)
+            famSuT = (famSu * 250)
+
+            nTotal = (sinRmT + douRmT + famSuT)
+            aTotal = (nTotal * duration)
+            VATTotal = (aTotal * 1.2)
+
+        End If
+
+        Console.WriteLine("So your total per night will be, £" & nTotal)
+        Console.WriteLine("Your total for the whole duration will be, £" & aTotal)
+        Console.WriteLine("Including taxes that will be, £" & VATTotal)
 
 
 
